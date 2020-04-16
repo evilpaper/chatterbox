413fa1d21251224afcbc6ce1d994bc3b4fb7b9d2
evilpaper <pellburt@gmail.com>
2020-04-16 20:48:32 +0200

    Setup basic functionality

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,30 @@ class MessageApp {
   constructor() {
     this.messages = [];
   }
+
+  post(content) {
+    const item = {
+      content: content,
+      date: new Date(),
+      id: this.messages.length,
+    };
+    this.messages.push(item);
+    return this.messages;
+  }
+
+  get(id) {
+    return this.messages[id];
+  }
+
+  update(id, content) {
+    this.messages[id].content = content;
+    return this.messages[id];
+  }
+
+  delete(id) {
+    this.messages.splice(id - 1, 1);
+    return this.messages;
+  }
 }
 
 export default MessageApp;
--- backend/app.test.js
+++ backend/app.test.js
@@ -2,8 +2,39 @@ import { expect } from "chai";
 import MessageApp from "./app.js";
 
 describe("app", function () {
-  let testApp = new MessageApp();
+  let testApp;
+
+  this.beforeEach(() => {
+    testApp = new MessageApp();
+    testApp.post("Hi World!");
+  });
+
   it("app has messages", function () {
     expect(testApp.messages).to.be.an("array");
+  });
+
+  it("app creates message (post)", function () {
+    testApp.post("Hi World!");
+    expect(testApp.messages.length).to.equal(2);
+  });
+
+  it("message has content, date and id", function () {
+    expect(testApp.messages[0].content).to.equal("Hi World!");
+    expect(testApp.messages[0].date).not.to.equal(undefined);
+    expect(testApp.messages[0].id).to.equal(0);
+  });
+
+  it("app reads (get)", function () {
+    expect(testApp.get(0).content).to.equal("Hi World!");
+  });
+
+  it("app updates (update)", function () {
+    testApp.update(0, "Hello World!");
+    expect(testApp.get(0).content).to.equal("Hello World!");
+  });
+
+  it("app deletes (delete)", function () {
+    testApp.delete(0);
+    expect(testApp.messages.length).to.equal(0);
   });
 });
