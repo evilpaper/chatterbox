c0cfba63f0e4e3e7b659b0d8052e1edcb4e2b314
evilpaper <pellburt@gmail.com>
2020-04-19 13:30:03 +0200

    Configure Express

--- backend/app.js
+++ backend/app.js
@@ -1,90 +1,10 @@
-import fs from "fs";
-import path from "path";
+import express from "express";
+const app = express();
 
-function getID(array) {
-  if (array.length > 0) {
-    return array[array.length - 1].id + 1;
-  } else {
-    return 1;
-  }
-}
+app.get("/", function (req, res) {
+  res.send({ val: "Hello World" });
+});
 
-class MessageApp {
-  constructor(filepath) {
-    this.filepath = filepath;
-    this.messages = filepath ? this.readFromJson() : [];
-  }
+app.listen(3001);
 
-  readFromJson() {
-    return JSON.parse(
-      fs.readFileSync(
-        __dirname + path.normalize(this.filepath),
-        "utf8",
-        (err, data) => {
-          if (err) throw err;
-        }
-      )
-    );
-  }
-
-  writeToJson() {
-    if (this.filepath) {
-      const jsonItem = JSON.stringify(this.messages);
-      fs.writeFileSync(
-        __dirname + path.normalize(this.filepath),
-        jsonItem,
-        (err) => {
-          if (err) throw err;
-        }
-      );
-    }
-  }
-
-  post(content) {
-    if (content) {
-      this.messages.push({
-        content: content,
-        date: new Date(),
-        id: getID(this.messages),
-      });
-      this.writeToJson();
-      return this.messages;
-    } else if (!content) {
-      return [];
-    }
-  }
-
-  get(id) {
-    return this.messages.filter((message) => message.id === id)[0];
-  }
-
-  getAll() {
-    return this.messages;
-  }
-
-  update(id, update) {
-    let index = this.messages.findIndex((message) => message.id == id);
-
-    if (index >= 0) {
-      this.messages[index].content = update;
-      this.writeToJson();
-      return this.messages[index];
-    } else {
-      return [];
-    }
-  }
-
-  delete(id) {
-    let index = this.messages.findIndex((message) => message.id == id);
-
-    if (index >= 0) {
-      this.messages.splice(index, 1);
-      this.writeToJson();
-      return this.messages;
-    } else {
-      return "Message not found in database";
-    }
-  }
-}
-
-export default MessageApp;
+export default app;
